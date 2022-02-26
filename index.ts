210cd687418fe9dd2886efa947f71d1ae68fb2f3
Antoine Balaine <abalaineaccts@gmail.com>
2022-02-25 17:13:00 -0800

    change folder structure, add file-creating functions

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("hello typescript");
+import { transposeOctUp } from "./src/transpose";
+const vscode = require('vscode');
+
+module.exports.macroCommands = {
+   OctUp: {
+      no: 1,
+      func: OctUp
+   },
+};
+
+const editor = vscode.window.activeTextEditor;
+
+function OctUp() {
+
+   if (!editor) {
+      // Return an error message if necessary.
+      return 'Editor is not opening.';
+   }
+   const document = editor.document;
+   const selection = editor.selection;
+   const text = document.getText(selection);
+   if (text.length > 0) {
+      editor.edit(( editBuilder: any ) => {
+         // To surround a selected text in double quotes(Multi selection is not supported).
+         editBuilder.replace(selection, transposeOctUp(text));
+      });
+   }
+}
+
+function WrapInTag(){
+   const currentLineNumber = editor.selection.active.line;
+   const currentLine = editor.lineAt(currentLineNumber);
+}
+
--- src/annotationsActions.ts
+++ src/annotationsActions.ts
@@ -1,3 +1,11 @@
+import {isFile, isDirectory} from 'path-type';
+import { promises as fs } from "fs";
+import path from "path";
+type abcText = string;
+export enum annotationCommandEnum {
+    createHarmonisationFile = "harmonisation",
+    createFamiliesFile = "families"
+}
 
 enum harmonisationStyles { 
     soli = "soli",
@@ -8,7 +16,7 @@ enum harmonisationStyles {
     cluster = "clstr"
 }
 
-enum instrumentFamily { 
+enum instrumentFamilies { 
     brass = "br",
     woodwinds = "wd",
     percussion = "prc",
@@ -25,3 +33,48 @@ enum instrumentFamily {
         copy the contents to instrumentFamilies file || copy the instrument tags to the harmony file
 test: expect(fs.existsSync('file.txt')).to.be.true
 */
+
+export const createOrUpdateHarmonizationRoutine = async (
+    abcText: abcText, 
+    annotationCommand: annotationCommandEnum,  
+    scoreFilePath: string
+) => {
+    let dirTarget = "";
+    if (await isFile(scoreFilePath)) {
+        // extract directory
+        dirTarget = `${path.basename(path.dirname(scoreFilePath))}.${annotationCommand}`;
+    } else if (await isDirectory(scoreFilePath)){
+        dirTarget = `${scoreFilePath}.${annotationCommand}` 
+    }
+    await fs.writeFile(dirTarget, abcText, "utf-8");
+    return;
+};
+
+const parseTags = (text: abcText)=>{
+    let tags = text.match(/(["])(?:(?=(\\?))\2.)*?\1/g);
+    const uniqueTags = [ ...new Set(tags?.map(tag=>tag.replace(/["\/]+/g, "")))];
+
+    // TODO à changer
+    let harmonyTechniquesFile = "";
+    let familyFile = ""
+
+    for (let i=0; i<uniqueTags.length; i++){
+
+        if (harmonyTechniquesFile && familyFile) break;
+
+        if ( Object.values(harmonisationStyles).includes(uniqueTags[i] as harmonisationStyles) && !harmonyTechniquesFile) {
+            //create harmonyTechniquesFile
+        }
+
+        if (Object.values(instrumentFamilies).includes(uniqueTags[i] as instrumentFamilies) && !familyFile){
+            //create familyFile;
+        }
+    /**
+     * write harmony to files:
+     * copy all the file, replace all the pitches that are outside of tags by rests
+     * consolidate slurred note's lengths (for midi conversion)
+     * realize chord symbols
+     * apply harmony techniques
+     */
+    }
+}
