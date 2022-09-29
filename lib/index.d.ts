8250f015b18eacccbc713d2489ae147c4e070f84
Antoine Balaine <abalaineaccts@gmail.com>
2022-09-29 16:12:02 -0700

    remove "lib" folder

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { consolidateRestsInRoutine, createInstrumentationRoutine } from "./src/annotationsActions";
-import { noteDispatcher } from "./src/dispatcher";
-import { reorderChordTransform } from "./src/transformChords";
-import { convertToEnharmoniaTransform, convertToRestTransform, octaviateDownTransform, octaviateUpTransform, transposeHalfStepDownTransform, transposeHalfStepUpTransform, transposeStepDownTransform, transposeStepUpTransform } from "./src/transformPitches";
-import { consolidateConsecutiveNotesTransform, divideLengthTransform, duplicateLengthTransform } from "./src/transformRests";
-export { consolidateConsecutiveNotesTransform, consolidateRestsInRoutine, convertToEnharmoniaTransform, convertToRestTransform, createInstrumentationRoutine, noteDispatcher, divideLengthTransform, duplicateLengthTransform, octaviateDownTransform, octaviateUpTransform, reorderChordTransform, transposeHalfStepDownTransform, transposeHalfStepUpTransform, transposeStepDownTransform, transposeStepUpTransform, };
